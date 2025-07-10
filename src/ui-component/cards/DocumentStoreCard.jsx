bfe348651e65b4c277c20013c2f8cc2d55527a90
anmol008 <anmolbhargava97@gmail.com>
2025-07-10 09:28:44 +0530

    changes

--- src/ui-component/cards/DocumentStoreCard.jsx
+++ src/ui-component/cards/DocumentStoreCard.jsx
@@ -13,16 +13,11 @@ import DocumentStoreStatus from '@/views/docstore/DocumentStoreStatus'
 import { kFormatter } from '@/utils/genericHelper'
 
 const CardWrapper = styled(MainCard)(({ theme }) => ({
-    background: theme.palette.card.main,
     color: theme.darkTextPrimary,
     overflow: 'auto',
     position: 'relative',
     boxShadow: '0 2px 14px 0 rgb(32 40 45 / 8%)',
     cursor: 'pointer',
-    '&:hover': {
-        background: theme.palette.card.hover,
-        boxShadow: '0 2px 14px 0 rgb(32 40 45 / 20%)'
-    },
     height: '100%',
     minHeight: '160px',
     maxHeight: '300px',
